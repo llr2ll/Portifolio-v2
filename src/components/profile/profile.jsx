1314f3bfbbcec3471dc2e2288b7a779828f7e18b
Raphae-Sanseverino <raphael.sanseverino@cosmospro.com.br>
2023-12-11 14:40:11 -0300

    checkpoint

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -1,21 +1,16 @@
-import { SiJavascript } from 'react-icons/si';
-import { SiElectron } from 'react-icons/si';
+import { FaNode, FaAngular, FaReact, FaVuejs } from 'react-icons/fa';
+import { SiElectron, SiJavascript } from 'react-icons/si';
 import img from '../../assets/profile.jpeg';
-import { FaAngular } from "react-icons/fa";
-import { FaNodeJs } from 'react-icons/fa';
-import { FaReact } from 'react-icons/fa';
 import { GrMysql } from 'react-icons/gr';
-import { FaVuejs } from "react-icons/fa";
 import { TbSql } from "react-icons/tb";
 import "./profile.css"
-
 
 export function Profile(){
     return <section id="home" className="container home-container">
         <div className="logo">
             <div className="hover-show active">
             <span className='circle'><FaReact /></span>
-            <span className='circle' ><FaNodeJs /></span>
+            <span className='circle' ><FaNode /></span>
             <span className='circle'><SiJavascript /></span>
             <span className='circle'><SiElectron /></span>
             <span className='circle'><TbSql /></span>
