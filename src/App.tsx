e14d9ff785d261d907ba1ef21dd525081ef4a2f7
llr2ll <raphaelsanseverino@gmail.com>
2023-12-11 22:25:22 -0300

    checkpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Background, Profile, Contact, Nav, Theme, Certificates} from './components';
+import { Background, Profile, Contact, Nav, Theme, Certificates, Skills } from './components';
 
 function App() {
   return <main>
@@ -6,6 +6,7 @@ function App() {
     <Nav/>
     <Theme />
     <Profile/>
+    <Skills />
     <Certificates/>
     <Contact/>
   </main>
--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -68,8 +68,6 @@ import Box from '@mui/material/Box';
 
 
 export function Skills(){
-    {/* <Grid container spacing={2}></Grid> */}
-    
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
         ...theme.typography.body2,
@@ -78,191 +76,16 @@ export function Skills(){
         color: theme.palette.text.secondary,
     }));
 
-    return <Box sx={{ width: '100%' }}>
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            <Grid item xs={6}>
-                <Item>
-                    
-                </Item>
-            </Grid>
-            <Grid item xs={6}>
-            <Item>2</Item>
-            </Grid>
-            <Grid item xs={6}>
-            <Item>3</Item>
-            </Grid>
-            <Grid item xs={6}>
-            <Item>4</Item>
-            </Grid>
+    return <Box sx={{ width: '100%', padding: "5%" }}>
+        <Grid container>
+            <Item item md={6}>
+                <FaHtml5 fontSize={"4rem"} />
+                <Item>HTML 5</Item>
+            </Item>
+            <Item item>
+                <FaMarkdown fontSize={"4rem"} />
+                <Item>MarkDown</Item>
+            </Item>
         </Grid>
     </Box>
 }
-
-
-
-
-
-
-
-
-
-
-/*
-[[ Linguagens de Marcação / Markup Languages / Lenguajes de marcado ]]
-
-    -HTML 5 	( Extremamente Avançado )           <FaHtml5 />
-    -MarkDown 	( Avançado )                        <FaMarkdown />  
-
-[[ Linguagens de estilização / Styling languages / Lenguajes de estilo ]]
-
-    -Css 	( extremamente Avançado )               <FaCss3 />
-    -Sass 	( avançado )                            <FaSass />
-    -Scss	( avançado )                            <BsFiletypeScss />
-
-[[ Linguagens de Programação / Programming languages / Lenguajes de programación ]]
-
-    -Javascript 	( Extremamente Avançado )       <SiJavascript />
-    -Php 	 	    ( intermediário )               <FaPhp />
-    -C#		        ( intermediário )               <TbBrandCSharp />
-    -Dart		    ( extremamente básico )         <SiDart />
-    -Lua		    ( Básico )                      <SiLua />
-
-[[ Linguagens de Transferencia de dados / Data Transfer Languages / Lenguajes de transferencia de datos ]]
-
-    -Json 	( Extremamente Avançado )               <SiJson />
-    -XML	( intermediário )                       <TbFileTypeXml />
-    -YAML	( Básico )                              <SiYaml />
-
-[[ Protocolos de comunicação / Communication protocols / Protocolos de comunicación ]]
-
-    -HTTP 	( Extremamente Avançado )               <MdHttp />
-
-[[ Ferramentas para teste de API / API Testing Tools / Herramientas de prueba API ]]
-
-    -Postman 	( Avançado )                        <SiPostman />
-    
-    
-    -Insominia 	( Avançado )
-    -Thunder Client ( Avançado )
-
-
-[[ Linguagens de banco de dados / Database languages / Lenguajes de bases de datos ]]
-
-    -MySQL 		( Avançado )                        <SiMysql />
-    -SQL SEVER 	( Bem Avançado )                    <SiMicrosoftsqlserver />
-    -MongoDB	( Básico )                          <SiMongodb />
-
-[[ Devops ]]
-
-    -Azure devops ( intermediário )                 <SiMicrosoftazure />
-    -Github ( Avançado )                            <FaGithub />
-
-[[ Versionamento de código / Code versioning / Versionado de código ]]
-
-    -Git ( Extremamente Avançado )                  <FaGitAlt />
-
-[[ Bibliotecas de Php / Php FrameWorks / Bibliotecas de PHP ]]
-
-    -Laravel        	( intermediário )           <FaLaravel />
-    -Composer 		    ( Básico )                  <SiComposer />
-    -Xamp			    ( Básico )                  <SiXampp />
-
-[[ Bibliotecas de Javascript / Javascript FrameWorks / Bibliotecas de Javascript ]]
-
-    -React*		( avançado )                        <FaReact />
-    -Angular*	( avançado )                        <FaAngular />
-    -Vue*		( intermediário )                   <FaVuejs />
-    -Node*	 	( avançado )                        <FaNode />
-    
-    
-    
-    -Express*	    ( intermediário )
-    
-    
-    -Redux		    ( intermediário )               <SiRedux />
-    -Next*	 	    ( básico )                      <SiNextdotjs />
-    -Jquery		    ( avançado )                    <SiJquery />
-    -Chart JS	    ( básico )                      <SiChartdotjs />
-    -Moment	JS	    ( básico )                      <BsClockHistory />
-    -Three JS	    ( Bem Básico )                  <SiThreedotjs />
-    -Electron	    ( Intermediário )               <SiElectron />
-    -React router   ( avançado )                    <SiReactrouter />
-    -Angular router ( avançado )                    <FaAngular />
-
-[[ Pré compiladores de javascript / javascript pre compilers / Precompiladores de Javascript ]]
-
-    -Babel 		    ( intermediário )               <SiBabel />
-    -Typescript*	( Bem Avançado )                <SiTypescript />
-
-    [[ Javascript Tools ]]
-
-    -ESLint  	    ( intermediário )               <SiEslint /> 
-    -Webpack	    ( intermediário )               <SiWebpack />
-    -Vite		    ( intermediário )               <SiVite />
-
-[[ Bibliotecas de Componentes Javscript /  Javascript Component FrameWorks / Bibliotecas de componentes Javascript ]]
-
-
-
-    -Devextreme 	( Bem Avançado )
-    -SyncFusion	    ( Bem Avançado )
-    -Material UI	( Bem Avançado )
-
-
-
-    -Bootstrap	    ( avançado )                    <FaBootstrap />
-
-[[ Javscript Icons FrameWorks ]]
-
-    -React Icons 		( Bem Avançado )
-    
-    -Font Awesome		( Bem Avançado )            <SiFontawesome />
-
-    -Materia UI Icons 	( Bem Avançado )
-
-[[ Sistemas Operacionais ]]
-
-    -Linux 		( intermediário )                   <FcLinux />
-    -Windows 	( Bem Avançado )                    <FaWindows />
-    
-[[ Distros de Linux ]]
-
-    -Arch Linux 	( intermediário )               <SiArchlinux />
-    -Ubuntu		    ( intermediário )               <FaUbuntu />
-    -Kali Linux	    ( Básico )                      <SiKalilinux />
-
-[[ Games Engines ]]
-
-    -Unreal Engine 4 ( intermediário )              <SiUnrealengine />
-    -Unity		     ( intermediário )              <FaUnity />
-
-[[ Criação 3D ]]
-
-    -Blender	( intermediário )                   <SiBlender />
-
-[[ Criação de Sites ]]
-
-    -Wix 			        ( Básico )              <FaWix />
-    -Wordpress	            ( intermediário )       <FaWordpress />
-    -Elementor          	( intermediário )       <SiElementor />
-    -Hostgator 		        ( Básico )
-
-[[ Editores de Imagem ]]
-
-    -Photshop	( intermediário )                   <SiAdobephotoshop />
-    -Canva		( intermediário )                   <SiCanva />
-    -Paint.NET	( Avançado )                        <GiPaintBrush />
-    -Krita		( Básico )                          <SiKrita />
-
-[[ Editores de vídeo ]]
-
-
--Sony Vegas 	( intermediário )
-
-
-    -Canva		( intermediário )                   <SiCanva />
-
-[[ Captura de tela / Live ]]
-
-    -OBS ( avançado )                               <SiObsstudio />
-*/
