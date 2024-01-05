0694ec19426dbd24b68ea70fc90ad45cf5e5d299
Raphael Sanseverino <raphael.sanseverino@cosmospro.com.br>
2024-01-05 18:13:49 -0300

    checkpoint

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -7,18 +7,35 @@ import "./profile.css"
 
 export function Profile(){
     return <section id="home" className="container home-container">
-        <div className="logo">
+        <div className="logo"> 
+        {/*  */}
             <div className="hover-show active showspin">
-                <span className='circle fade-out'><FaReact /></span>
-                <span className='circle fade-out'><FaNode /></span>
-                <span className='circle fade-out'><SiJavascript /></span>
-                <span className='circle fade-out'><SiElectron /></span>
-                <span className='circle fade-out'><TbSql /></span>
-                <span className='circle fade-out'><FaVuejs /></span>
-                <span className='circle fade-out'><FaAngular /></span>
-                <span className='circle fade-out'><GrMysql /></span>
+                <span className='circle fade-out'>
+                    {/* <FaReact /> */}
+                </span>
+                <span className='circle fade-out'>
+                    {/* <FaNode /> */}
+                </span>
+                <span className='circle fade-out'>
+                    {/* <SiJavascript /> */}
+                </span>
+                <span className='circle fade-out'>
+                    {/* <SiElectron /> */}
+                </span>
+                <span className='circle fade-out'>
+                    {/* <TbSql /> */}
+                </span>
+                <span className='circle fade-out'>
+                    {/* <FaVuejs /> */}
+                </span>
+                <span className='circle fade-out'>
+                    {/* <FaAngular /> */}
+                </span>
+                <span className='circle fade-out'>
+                    {/* <GrMysql /> */}
+                </span>
             </div>
-            <img src={img} className='pulse-border-profile'/>
+            {/* <img src={img} className='pulse-border-profile'/> */}
         </div>
     </section>
 }
